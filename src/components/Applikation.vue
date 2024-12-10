e1e3eab9dd278fb54b18afba24fcb0a54d06750f
Anders Julfeldt Petersen <ajp@nine.dk>
2024-12-10 14:27:47 +0100

    ERF-9864: tilføj support for multiple ejere af tekstnøgle bundt

--- src/components/Applikation.vue
+++ src/components/Applikation.vue
@@ -3,6 +3,7 @@
   <VgDesignWrapper>
     <div class="row">
       <div class="col-12 col-md-10">
+        x: {{ tekstnoegleCvrNumre }}
         <h1>Demo leverandør-applikation</h1>
         <p class="my-5">
           Denne applikation kan bruges som skabelon til udvikling af leverandør-applikation, der kan integreres i
